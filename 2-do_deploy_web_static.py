03cde922c878cbe6b61ac7494be4e3e6aa5c36cc
audusunday <sunnadu13@gmail.com>
2023-07-14 12:51:57 +0100

    update on task3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['52.91.133.232', '18.233.64.139']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['52.91.133.232', '18.233.64.139']
 
 
 def do_pack():
